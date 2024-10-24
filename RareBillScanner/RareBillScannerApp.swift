65185b332fe2c7c3ebc877620febca44e712d8ac
Mateusz Gozdzik <mateusz@gozdzik.xyz>
2024-10-24 21:52:18 +0200

    Initial Commit

---
+++ RareBillScanner/RareBillScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RareBillScannerApp.swift
+//  RareBillScanner
+//
+//  Created by Mateusz Gozdzik on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RareBillScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
